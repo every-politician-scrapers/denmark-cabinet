0cdcf4d69bffd328b322bd4d07fc9ab33d645326
Tony Bowden <tony@tmtm.com>
2021-07-28 12:49:29 +0300

    Initial scrapers and data

---
+++ bin/scraper/official.rb
@@ -0,0 +1,40 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class MemberList
+  # details for an individual member
+  class Member < Scraped::HTML
+    field :name do
+      tds[0].css('img/@alt').text.tidy
+    end
+
+    field :position do
+      tds[1].xpath('text()').first.text.tidy
+    end
+
+    private
+
+    def tds
+      noko.css('td')
+    end
+  end
+
+  # The page listing all the members
+  class Members < Scraped::HTML
+    field :members do
+      member_container.map { |member| fragment(member => Member).to_h }
+    end
+
+    private
+
+    def member_container
+      noko.css('table.ankiro-results').xpath('.//tr[td[@class="cellPic"]]')
+    end
+  end
+end
+
+file = Pathname.new 'html/official.html'
+puts EveryPoliticianScraper::FileData.new(file).csv
